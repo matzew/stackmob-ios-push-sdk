3a3f7a274fb3b5c61aee4fec04a0ebd31d3024fa
Douglas Rapp <drapp@stackmob.com>
2012-08-06 12:08:00 -0700

    gettokensforusers tests

--- integration-tests/SMPushClientIntegrationSpec.m
+++ integration-tests/SMPushClientIntegrationSpec.m
@@ -188,12 +188,12 @@ describe(@"SMPushClient", ^{
             });
             [[theValue(succeeded) should] beYes];
         });
-        it(@"should register and delete an Android token flag", ^{
+        it(@"should register and delete an Android token", ^{
             __block BOOL succeeded = NO;
             __block SMPushToken *token = [[SMPushToken alloc] initWithString:@"helloworld" type:TOKEN_TYPE_ANDROID_GCM];
             syncWithSemaphore(^(dispatch_semaphore_t semaphore) {
 
-                [defaultClient registerDeviceToken:token withUser:@"herc" onSuccess:^{
+                [defaultClient registerDeviceToken:token withUser:@"kodi" onSuccess:^{
                     succeeded = YES;
                     syncReturn(semaphore);
                 } onFailure:^(NSError *theError) {
@@ -230,7 +230,123 @@ describe(@"SMPushClient", ^{
         });
     });
     describe(@"get tokens for users", ^{
-
+        it(@"should succeed with no results for an nonexistent user", ^{
+            __block BOOL succeeded = NO;
+            __block NSDictionary *result;
+            syncWithSemaphore(^(dispatch_semaphore_t semaphore) {
+                [defaultClient getTokensForUsers:[NSArray arrayWithObject:@"herc"] onSuccess:^(NSDictionary * tokens){
+                    succeeded = YES;
+                    result = tokens;
+                    syncReturn(semaphore);
+                } onFailure:^(NSError *theError) {
+                    succeeded = NO;
+                    syncReturn(semaphore);
+                }];
+            });
+            [[theValue(succeeded) should] beYes];
+            NSArray *tokenList = [result valueForKey:@"herc"];
+            [[theValue([tokenList count]) should] equal:[NSNumber numberWithInt:0]];
+        });
+        it(@"should succeed for an existing user", ^{
+            __block BOOL succeeded = NO;
+            __block NSDictionary *result;
+            syncWithSemaphore(^(dispatch_semaphore_t semaphore) {
+                [defaultClient getTokensForUsers:[NSArray arrayWithObject:@"nola"] onSuccess:^(NSDictionary * tokens){
+                    succeeded = YES;
+                    result = tokens;
+                    syncReturn(semaphore);
+                } onFailure:^(NSError *theError) {
+                    succeeded = NO;
+                    syncReturn(semaphore);
+                }];
+            });
+            [[theValue(succeeded) should] beYes];
+            NSArray *tokenList = [result valueForKey:@"nola"];
+            [[theValue([tokenList count]) should] equal:[NSNumber numberWithInt:1]];
+            SMPushToken *token = [tokenList objectAtIndex:0];
+            [[token.tokenString should] equal:token2];
+            [[token.type should] equal:TOKEN_TYPE_IOS];
+            [theValue(token.registeredMilliseconds) shouldNotBeNil];
+        });
+        it(@"should succeed for an existing user with multiple tokens", ^{
+            __block BOOL succeeded = NO;
+            __block NSDictionary *result;
+            syncWithSemaphore(^(dispatch_semaphore_t semaphore) {
+                [defaultClient getTokensForUsers:[NSArray arrayWithObject:@"bodie"] onSuccess:^(NSDictionary * tokens){
+                    succeeded = YES;
+                    result = tokens;
+                    syncReturn(semaphore);
+                } onFailure:^(NSError *theError) {
+                    succeeded = NO;
+                    syncReturn(semaphore);
+                }];
+            });
+            [[theValue(succeeded) should] beYes];
+            NSArray *tokenList = [result valueForKey:@"bodie"];
+            [[theValue([tokenList count]) should] equal:[NSNumber numberWithInt:2]];
+            SMPushToken *firstToken = [tokenList objectAtIndex:0];
+            SMPushToken *secondToken = [tokenList objectAtIndex:1];
+            if ([[firstToken tokenString] isEqualToString:token1]) {
+                SMPushToken *tempToken = firstToken;
+                firstToken = secondToken;
+                secondToken = tempToken;
+            }
+            [[firstToken.tokenString should] equal:token0];
+            [[firstToken.type should] equal:TOKEN_TYPE_IOS];
+            [theValue(firstToken.registeredMilliseconds) shouldNotBeNil];
+            [[secondToken.tokenString should] equal:token1];
+            [[secondToken.type should] equal:TOKEN_TYPE_IOS];
+            [theValue(secondToken.registeredMilliseconds) shouldNotBeNil];
+        });
+        
+        it(@"should succeed for an android token", ^{
+            
+            __block BOOL succeeded = NO;
+            __block SMPushToken *token = [[SMPushToken alloc] initWithString:@"helloworld" type:TOKEN_TYPE_ANDROID_GCM];
+            syncWithSemaphore(^(dispatch_semaphore_t semaphore) {
+                
+                [defaultClient registerDeviceToken:token withUser:@"kodi" onSuccess:^{
+                    succeeded = YES;
+                    syncReturn(semaphore);
+                } onFailure:^(NSError *theError) {
+                    succeeded = NO;
+                    syncReturn(semaphore);
+                }];
+            });
+            [[theValue(succeeded) should] beYes];
+            succeeded = NO;
+            __block NSDictionary *result;
+            syncWithSemaphore(^(dispatch_semaphore_t semaphore) {
+                [defaultClient getTokensForUsers:[NSArray arrayWithObject:@"kodi"] onSuccess:^(NSDictionary * tokens){
+                    succeeded = YES;
+                    result = tokens;
+                    syncReturn(semaphore);
+                } onFailure:^(NSError *theError) {
+                    succeeded = NO;
+                    syncReturn(semaphore);
+                }];
+            });
+            [[theValue(succeeded) should] beYes];
+            NSArray *tokenList = [result valueForKey:@"kodi"];
+            [[theValue([tokenList count]) should] equal:[NSNumber numberWithInt:1]];
+            SMPushToken *firstToken = [tokenList objectAtIndex:0];
+            [[firstToken.tokenString should] equal:[token tokenString]];
+            [[firstToken.type should] equal:[token type]];
+            [theValue(firstToken.registeredMilliseconds) shouldNotBeNil];
+            
+            __block BOOL deleteSuccess = NO;
+            syncWithSemaphore(^(dispatch_semaphore_t semaphore) {
+                [defaultClient deleteToken:token onSuccess:^{
+                    deleteSuccess = YES;
+                    syncReturn(semaphore);
+                } onFailure:^(NSError *theError) {
+                    deleteSuccess = NO;
+                    syncReturn(semaphore);
+                }];
+            });
+            
+            [[theValue(deleteSuccess) should] beYes];
+        });
     });
 
         
