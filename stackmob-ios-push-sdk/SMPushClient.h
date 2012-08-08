fec3cfb07ee016ad1bff33ec155d4be0291fa067
Douglas Rapp <drapp@stackmob.com>
2012-08-08 11:08:54 -0700

    Tidy up these docs

--- stackmob-ios-push-sdk/SMPushClient.h
+++ stackmob-ios-push-sdk/SMPushClient.h
@@ -35,11 +35,12 @@ typedef void (^SMResultsSuccessBlock)(NSArray *results);
 typedef void (^SMFailureBlock)(NSError *error);
 
 /**
- * A SMPushClient provides a high level interface to interacting with StackMob's push service. A new client must be given an APIVersion and OAuth credentials in order to communicate with your StackMob application.
- *
- * SMPushClient sets default values for other configuration settings which may be set as necessary by your application.
- *
- * SMPushClient exposes a defaultClient for applications which use a globally available client to share configuration settings.
+ A SMPushClient provides a high level interface to interacting with StackMob's push service. A new client must be given an APIVersion and OAuth credentials in order to communicate with your StackMob application.
+ 
+ SMPushClient sets default values for other configuration settings which may be set as necessary by your application.
+ 
+ SMPushClient exposes a defaultClient for applications which use a globally available client to share configuration settings.
+ 
  */
 @interface SMPushClient : NSObject
 
@@ -49,17 +50,23 @@ typedef void (^SMFailureBlock)(NSError *error);
 @property(nonatomic, readonly, copy) NSString *host;
 
 /**
- * @param APIVersion The API version of your StackMob application which this client instance should use.
- * @param publicKey Your StackMob application's OAuth publicKey.
- * @param privateKey Your StackMob application's OAuth privateKey.
+ Initialize with only the most basic parameters and defaults for the rest.
+ 
+ @param APIVersion The API version of your StackMob application which this client instance should use.
+ @param publicKey Your StackMob application's OAuth publicKey.
+ @param privateKey Your StackMob application's OAuth privateKey.
+ 
  */
 - (id)initWithAPIVersion:(NSString *)appAPIVersion publicKey:(NSString *)publicKey privateKey:(NSString *)privateKey;
 
 /**
- * @param APIVersion The API version of your StackMob application which this client instance should use.
- * @param publicKey Your StackMob application's OAuth publicKey.
- * @param privateKey Your StackMob application's OAuth privateKey.
- * @param pushHost The url of StackMob's push server
+ Initialize specifying all parameters.
+ 
+ @param APIVersion The API version of your StackMob application which this client instance should use.
+ @param publicKey Your StackMob application's OAuth publicKey.
+ @param privateKey Your StackMob application's OAuth privateKey.
+ @param pushHost The url of StackMob's push server
+ 
  */
 - (id)initWithAPIVersion:(NSString *)appAPIVersion publicKey:(NSString *)publicKey privateKey:(NSString *)privateKey pushHost:(NSString *)host;
 
@@ -69,75 +76,87 @@ typedef void (^SMFailureBlock)(NSError *error);
 + (SMPushClient *)defaultClient;
 
 /** 
- * Register a device token for push notifications. Device tokens must be registered before they
- * before they can be pushed to. Each token is associated with a username on the server, and this
- * can be used to simplify pushing to users on multiple devices and platforms via 
- * sendMessage:toUsers:onSuccess:onFailure:. The username can correspond to a regular StackMob
- * username, but it doesn't have to. You can also ignore usernames altogether by specifying an 
- * arbitrary value and working entirely with tokens. This call will fail if the token is already
- * registered.
- * @param token The device token for this device
- * @param username A name to be associated with the 
- * @param onSuccess The block to call on success
- * @param onFailure The block to call on failure
+ Register a device token for push notifications. 
+ 
+ Device tokens must be registered before they can be pushed to. Each token is associated with a username on the server, and this can be used to simplify pushing to users on multiple devices and platforms via sendMessage:toUsers:onSuccess:onFailure:. The username can correspond to a regular StackMob username, but it doesn't have to. You can also ignore usernames altogether by specifying an arbitrary value and working entirely with tokens. This call will fail if the token is already registered.
+ 
+ @param token The device token for this device
+ @param username A name to be associated with the 
+ @param onSuccess The block to call on success
+ @param onFailure The block to call on failure
+ 
  */
 - (void)registerDeviceToken:token withUser:(NSString *)username onSuccess:(SMSuccessBlock)successBlock onFailure:(SMFailureBlock)failureBlock;
 
 /** 
- * Register a device token for push notifications. Device tokens must be registered before they
- * before they can be pushed to. Each token is associated with a username on the server, and this
- * can be used to simplify pushing to users on multiple devices and platforms via 
- * sendMessage:toUsers:onSuccess:onFailure:. The username can correspond to a regular StackMob
- * username, but it doesn't have to. You can also ignore usernames altogether by specifying an 
- * arbitrary value and working entirely with tokens. This call will fail if the token is already
- * registered unless the overwrite flag is set.
- * @param token The device token for this device
- * @param username A name to be associated with the 
- * @param overwrite When set registering an existing token will succeed
- * @param onSuccess The block to call on success
- * @param onFailure The block to call on failure
+ Register a device token for push notifications. 
+ 
+ Device tokens must be registered before they can be pushed to. Each token is associated with a username on the server, and this can be used to simplify pushing to users on multiple devices and platforms via sendMessage:toUsers:onSuccess:onFailure:. The username can correspond to a regular StackMob username, but it doesn't have to. You can also ignore usernames altogether by specifying an arbitrary value and working entirely with tokens. This call will fail if the token is already registered unless the overwrite flag is set.
+ 
+ @param token The device token for this device
+ @param username A name to be associated with the 
+ @param overwrite When set registering an existing token will succeed
+ @param onSuccess The block to call on success
+ @param onFailure The block to call on failure
+ 
  */
 - (void)registerDeviceToken:token withUser:(NSString *)username overwrite:(BOOL)overwrite onSuccess:(SMSuccessBlock)successBlock onFailure:(SMFailureBlock)failureBlock;
 
 /** 
- * Broadcast a message to all registered devices for this app. The message will be sent to all users on all platforms.
- * @param message The message to send. See [Apple's docs](http://developer.apple.com/library/mac/#documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/ApplePushService/ApplePushService.html) for details on format.
- * @param onSuccess The block to call on success
- * @param onFailure The block to call on failure
+ Broadcast a message to all registered devices for this app. 
+ 
+ The message will be sent to all users on all platforms.
+ 
+ @param message The message to send. See [Apple's docs](http://developer.apple.com/library/mac/#documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/ApplePushService/ApplePushService.html) for details on format.
+ @param onSuccess The block to call on success
+ @param onFailure The block to call on failure
+ 
  */
 - (void)broadcastMessage:(NSDictionary *)message onSuccess:(SMSuccessBlock)successBlock onFailure:(SMFailureBlock)failureBlock;
 
 /**
- * Send a message to a particular set of users previously registered by registerDeviceToken:withUser:onSuccess:onFailure:  
- * @param message The message to send. See [Apple's docs](http://developer.apple.com/library/mac/#documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/ApplePushService/ApplePushService.html) for details on format.
- * @param users An array of username strings to push to.
- * @param onSuccess The block to call on success
- * @param onFailure The block to call on failure
+ Send a message to a particular set of users 
+ 
+ The users must have been previously registered by registerDeviceToken:withUser:onSuccess:onFailure:  
+ 
+ @param message The message to send. See [Apple's docs](http://developer.apple.com/library/mac/#documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/ApplePushService/ApplePushService.html) for details on format.
+ @param users An array of username strings to push to.
+ @param onSuccess The block to call on success
+ @param onFailure The block to call on failure
+ 
  */
 - (void)sendMessage:(NSDictionary *)message toUsers:(NSArray *)users onSuccess:(SMSuccessBlock)successBlock onFailure:(SMFailureBlock)failureBlock;
 
 /**
- * Send a message to specific tokens. The tokens must have been registered with StackMob by registerDeviceToken:withUser:onSuccess:onFailure:  
- * @param message The message to send. See [Apple's docs](http://developer.apple.com/library/mac/#documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/ApplePushService/ApplePushService.html) for details on format.
- * @param tokens An array containing either device token strings or SMPushToken objects to push to
- * @param onSuccess The block to call on success
- * @param onFailure The block to call on failure
+ Send a message to specific tokens. 
+ 
+ The tokens must have been registered with StackMob by registerDeviceToken:withUser:onSuccess:onFailure:  
+ 
+ @param message The message to send. See [Apple's docs](http://developer.apple.com/library/mac/#documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/ApplePushService/ApplePushService.html) for details on format.
+ @param tokens An array containing either device token strings or SMPushToken objects to push to
+ @param onSuccess The block to call on success
+ @param onFailure The block to call on failure
+ 
  */
 - (void)sendMessage:(NSDictionary *)message toTokens:(NSArray *)tokens onSuccess:(SMSuccessBlock)successBlock onFailure:(SMFailureBlock)failureBlock;
 
 /**
- * Get the tokens registered for the given users. If successful the onSuccess block will be called with an NSDictionary mapping username strings to NSArrays of SMPushToken objects 
- * @param users An array of username strings to query
- * @param onSuccess The block to call on success
- * @param onFailure The block to call on failure
+ Get the tokens registered for the given users. If successful the onSuccess block will be called with an NSDictionary mapping username strings to NSArrays of SMPushToken objects 
+ 
+ @param users An array of username strings to query
+ @param onSuccess The block to call on success
+ @param onFailure The block to call on failure
+ 
  */
 - (void)getTokensForUsers:(NSArray *)users onSuccess:(SMResultSuccessBlock)successBlock onFailure:(SMFailureBlock)failureBlock;
 
 /**
- * Deletes the token from StackMob's server
- * @param token A token represented by either a device token string or an SMPushToken object
- * @param onSuccess The block to call on success
- * @param onFailure The block to call on failure
+ 
+ Deletes the token from StackMob's server
+ 
+ @param token A token represented by either a device token string or an SMPushToken object
+ @param onSuccess The block to call on success
+ @param onFailure The block to call on failure
  */
 - (void)deleteToken:(id)token onSuccess:(SMSuccessBlock)successBlock onFailure:(SMFailureBlock)failureBlock;
 
--- stackmob-ios-push-sdk/SMPushToken.h
+++ stackmob-ios-push-sdk/SMPushToken.h
@@ -12,6 +12,14 @@
 #define TOKEN_TYPE_ANDROID_C2DM @"android"
 #define TOKEN_TYPE_ANDROID_GCM @"androidgcm"
 
+/**
+ Represents the info needed to register a device for push with StackMob
+ 
+ This corresponds to what's sored about each token on server: token string paired with a platform and some supplemental information. 
+ 
+ @note In most cases you'll want to just use the device token string you get from apple directly rather than using one of these.
+ 
+ */
 @interface SMPushToken : NSObject
 
 @property(nonatomic, copy) NSString *tokenString;
@@ -19,7 +27,19 @@
 @property(nonatomic) long registeredMilliseconds;
 
 
+/**
+ Initialize with a device token string. Assumes the type is ios
+ 
+ @param tokenString The device token string.
+ */
 -(id)initWithString:(NSString *)tokenString;
+
+/**
+ Initialize with a device token string and a type.
+ 
+ @param tokenString The device token string.
+ @param type The platform the token is for. Should be one of ios, android, or androidgcm
+ */
 -(id)initWithString:(NSString *)tokenString type:(NSString *)type;
 
 
